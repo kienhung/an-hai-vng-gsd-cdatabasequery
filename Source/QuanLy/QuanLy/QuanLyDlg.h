9c3f0385b1b284e0e640e1564ef8526512b50e83
thhai132@gmail.com <thhai132@gmail.com@e1a6d249-4361-c319-3d58-ccd68b8f228d>
2012-07-10 16:32:46 +0000

    thêm list control

--- Source/QuanLy/QuanLy/QuanLyDlg.h
+++ Source/QuanLy/QuanLy/QuanLyDlg.h
@@ -14,19 +14,24 @@ public:
 
 // Dialog Data
 	enum { IDD = IDD_QUANLY_DIALOG };
-
+	CListCtrl	m_lstNhanVien;
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
 
 
 // Implementation
 protected:
-	HICON m_hIcon;
+	void InitListCtrlCols();
 
+	HICON m_hIcon;
+	
 	// Generated message map functions
 	virtual BOOL OnInitDialog();
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnAdd();
+	afx_msg void OnBnClickedBtnEdit();
 };
