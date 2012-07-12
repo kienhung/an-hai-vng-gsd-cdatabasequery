2a7a2a92f865168e6ae4ca5f3d67807e361a0b48
thienan289@gmail.com <thienan289@gmail.com@e1a6d249-4361-c319-3d58-ccd68b8f228d>
2012-07-12 09:37:05 +0000

    nhanviendao

--- Source/QuanLy/QuanLy/NhanVienDAO.h
+++ Source/QuanLy/QuanLy/NhanVienDAO.h
@@ -7,10 +7,10 @@ class CNhanVienDAO
 {
 public:
 	CNhanVienDAO(void);
-	~CNhanVienDAO(void);
+	virtual ~CNhanVienDAO(void);
 
 	virtual size_t GetAll(list<NHANVIEN> *plstNhanVien) = 0;
-	virtual bool AddNhanVien(const NHANVIEN &nhanVien) = 0;
-	virtual bool UpdateNhanVien(const NHANVIEN &nhanVien) = 0;
+	virtual bool AddNhanVien(const NHANVIEN &nhanVien, int iMaPhongBan) = 0;
+	virtual bool UpdateNhanVien(const NHANVIEN &nhanVien, int iMaPhongBan) = 0;
 	virtual bool DeleteNhanVien(int iMaNhanVien) = 0;
 };
