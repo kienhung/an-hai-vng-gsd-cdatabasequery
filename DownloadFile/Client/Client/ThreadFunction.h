2276fbe4ae4a1a6e11b269a4cf9fbcb9d5012983
thienan289 <thienan289@gmail.com>
2012-07-16 05:23:32 +0000

    xong ham client( con code cung)

--- DownloadFile/Client/Client/ThreadFunction.h
+++ DownloadFile/Client/Client/ThreadFunction.h
@@ -1,2 +1,7 @@
 #pragma once
 DWORD WINAPI SocketCommunicationThreadFunction(LPVOID lpParam );
+bool SendDownloadFileMessage(SOCKET sConnectSocket, const TCHAR strFileName[], int *piLastError);
+bool ReceiveFileSize(SOCKET sConnectSocket, unsigned __int64 *pi64FizeSize, int *piLastError);
+bool CheckFileExist(SOCKET sConnectSocket, int *piLastError);
+
+bool WriteData(SOCKET sConnectSocket, const TCHAR strFileName[], const unsigned __int64 &i64FizeSize, int *piLastError);
