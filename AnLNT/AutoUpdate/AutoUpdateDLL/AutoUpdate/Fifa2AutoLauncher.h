0b953a76fbbf2822a66ebe2b12c3a23c425a4510
thienan289@gmail.com <thienan289@gmail.com@e1a6d249-4361-c319-3d58-ccd68b8f228d>
2012-08-27 03:28:59 +0000

    hoan thanh game fifa

--- AnLNT/AutoUpdate/AutoUpdateDLL/AutoUpdate/Fifa2AutoLauncher.h
+++ AnLNT/AutoUpdate/AutoUpdateDLL/AutoUpdate/Fifa2AutoLauncher.h
@@ -5,16 +5,32 @@ class CFifa2AutoLauncher :
 	public CLauncher
 {
 protected:
-	int iAcceptCheckboxID;
-	int iZone1RadioButtonID;
-	int iStartButtonID;
+	int m_iAcceptCheckboxID;
+	int m_iZone1RadioButtonID;
+	int m_iStartButtonID;
+
+	int m_iFailedOkButtonID;
+
+	CString m_strWindowName;
+	CString m_strClassName;
+
+	enum STATE {CONNECT, DISCONNECT, UNKNOWN};
+	STATE m_state;
 public:
 
 	CFifa2AutoLauncher(LPCTSTR strSource);
 
 	virtual BOOL Run();
 	virtual CString GetName();
-	BOOL WaitForComplete();
+	
 
 	virtual ~CFifa2AutoLauncher(void);
+
+	static BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam );
+	static BOOL CALLBACK EnumChildWindowsProc(HWND hwnd, LPARAM lParam );
+
+protected:
+	BOOL WaitForComplete();
+	HWND StartLauncherWindow();
+	//BOOL KillMainWindowProcess();
 };
